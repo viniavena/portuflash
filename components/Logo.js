1d8e65248290bd651515a436beb5b74bf95dd4a0
Vinicius Avena <vini.avena@live.com>
2023-08-16 02:22:30 -0300

    deploy

--- components/Logo.js
+++ components/Logo.js
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Logo = () => {
   return (
     <div className="flex justify-center my-6 mb-14">
-      <img src="/negative-logo.svg" alt="Logo" />
+      <Image src="/negative-logo.svg" alt="Logo" />
     </div>
   );
 };
