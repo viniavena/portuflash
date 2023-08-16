a868df242a84a6736ef4cd5fbc8dfb58ca071113
Vinicius Avena <vini.avena@live.com>
2023-08-16 02:31:05 -0300

    fixing module speechword not found

--- components/Flashcards/FlashcardDeck.js
+++ components/Flashcards/FlashcardDeck.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 import FlashcardBack from './FlashcardBack';
 import FlashcardFront from './FlashcardFront';
 import LoadingSpinner from '../LoadingSpinner';
